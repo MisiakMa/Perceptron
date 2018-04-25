c9a514c9024b2c4d96fa4632049351918a289c6b
Sir3nka <adriandugosz277@gmail.com>
2018-04-25 22:11:38 +0200

    Beng

--- cmake-build-debug/CMakeFiles/iad2_projekt.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/iad2_projekt.dir/DependInfo.cmake
@@ -4,12 +4,12 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/pobi/Downloads/iad2-projekt/project/src/Aplikacja.cpp" "/home/pobi/Downloads/iad2-projekt/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Aplikacja.cpp.o"
-  "/home/pobi/Downloads/iad2-projekt/project/src/Dane.cpp" "/home/pobi/Downloads/iad2-projekt/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Dane.cpp.o"
-  "/home/pobi/Downloads/iad2-projekt/project/src/Funkcja.cpp" "/home/pobi/Downloads/iad2-projekt/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Funkcja.cpp.o"
-  "/home/pobi/Downloads/iad2-projekt/project/src/Losowanie.cpp" "/home/pobi/Downloads/iad2-projekt/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Losowanie.cpp.o"
-  "/home/pobi/Downloads/iad2-projekt/project/src/Siec.cpp" "/home/pobi/Downloads/iad2-projekt/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Siec.cpp.o"
-  "/home/pobi/Downloads/iad2-projekt/project/src/Warstwa.cpp" "/home/pobi/Downloads/iad2-projekt/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Warstwa.cpp.o"
+  "/home/pobi/Downloads/Perceptron/project/src/Aplikacja.cpp" "/home/pobi/Downloads/Perceptron/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Aplikacja.cpp.o"
+  "/home/pobi/Downloads/Perceptron/project/src/Dane.cpp" "/home/pobi/Downloads/Perceptron/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Dane.cpp.o"
+  "/home/pobi/Downloads/Perceptron/project/src/Funkcja.cpp" "/home/pobi/Downloads/Perceptron/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Funkcja.cpp.o"
+  "/home/pobi/Downloads/Perceptron/project/src/Losowanie.cpp" "/home/pobi/Downloads/Perceptron/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Losowanie.cpp.o"
+  "/home/pobi/Downloads/Perceptron/project/src/Siec.cpp" "/home/pobi/Downloads/Perceptron/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Siec.cpp.o"
+  "/home/pobi/Downloads/Perceptron/project/src/Warstwa.cpp" "/home/pobi/Downloads/Perceptron/cmake-build-debug/CMakeFiles/iad2_projekt.dir/project/src/Warstwa.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
